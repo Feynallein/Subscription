63ef4ec27ed38b2284be90390aa8c87fc06e1d9f
Martin Berthier <martin.berthier.mb@gmail.com>
2022-11-12 19:53:35 +0100

    improving subscriber

--- lib/src/subscriber.dart
+++ lib/src/subscriber.dart
@@ -2,12 +2,6 @@ import '../subscription.dart';
 
 mixin Subscriber {
   void notify<T>(T notification);
-
-  void subscribe<T>(Subscribee subscribee){
-    subscribee.subscriptions.update(T, (subscribers) => subscribers..add(this), ifAbsent: () => [this]);
-  }
-
-  void unSubscribe<T>(Subscribee subscribee){
-    subscribee.subscriptions[T]?.remove(this);
-  }
+  void subscribe<T>(Subscribee subscribee) => subscribee.subscriptions.update(T, (subscribers) => subscribers..add(this), ifAbsent: () => [this]);
+  void unSubscribe<T>(Subscribee subscribee) => subscribee.subscriptions[T]?.remove(this);
 }
