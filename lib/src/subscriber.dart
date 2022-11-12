73c278dbb62184923abd8a66c513ab1b6715200f
Martin Berthier <martin.berthier.mb@gmail.com>
2022-11-12 20:49:52 +0100

    removing typo uppercase

--- lib/src/subscriber.dart
+++ lib/src/subscriber.dart
@@ -3,5 +3,5 @@ import '../subscription.dart';
 mixin Subscriber {
   void notify<T>(T notification);
   void subscribe<T>(Subscribee subscribee) => subscribee.subscriptions.update(T, (subscribers) => subscribers..add(this), ifAbsent: () => [this]);
-  void unSubscribe<T>(Subscribee subscribee) => subscribee.subscriptions[T]?.remove(this);
+  void unsubscribe<T>(Subscribee subscribee) => subscribee.subscriptions[T]?.remove(this);
 }
